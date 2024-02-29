dbecccc11a0e75775d1f203970da389d19480ac1
Luigi <45099401+ProfDrLuigi@users.noreply.github.com>
2024-02-29 13:45:31 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,11 +22,11 @@ let package = Package(
             targets: ["WiredSwift"])
     ],
     dependencies: [
-        .package(name: "AEXML", url: "https://github.com/tadija/AEXML.git", from: "4.5.0"),
-        .package(name: "CryptoSwift", url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.3.0"),
-        .package(name: "CryptorRSA", url: "https://github.com/IBM-Swift/BlueRSA", from: "1.0.35"),
+        .package(name: "AEXML", url: "https://github.com/tadija/AEXML.git", from: "4.7.0"),
+        .package(name: "CryptoSwift", url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.8.1"),
+        .package(name: "CryptorRSA", url: "https://github.com/IBM-Swift/BlueRSA", from: "1.0.201"),
         .package(name: "CZlib", url: "https://github.com/IBM-Swift/CZlib.git", from: "0.1.2"),
-        .package(name: "Gzip", url: "https://github.com/1024jp/GzipSwift", from: "5.1.1"),
+        .package(name: "Gzip", url: "https://github.com/1024jp/GzipSwift", from: "6.0.1"),
         .package(name: "SocketSwift", url: "https://github.com/BiAtoms/Socket.swift.git", from: "2.4.0"),
     ],
     targets: [
